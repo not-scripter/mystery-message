4e4f8ab824b73c6dfb8c8bb16ef3ba7d4be93ef6
MRINMOY MONDAL <mm2922894@gmail.com>
2024-05-28 21:14:11 +0530

    test

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -68,3 +68,5 @@ export async function GET(request: Request) {
     );
   }
 }
+
+export const dynamic = "force-dynamic";
--- src/middleware.ts
+++ src/middleware.ts
@@ -15,11 +15,11 @@ export async function middleware(request: NextRequest) {
       url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/verify"))
   ) {
-    return NextResponse.redirect(new URL("/dashboard", url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", url));
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
